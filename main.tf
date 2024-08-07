c4b9ca0126c810e018d1f3e50ab7ff47ad9acfc5
root <root@LAPTOP-8SB38NT7>
2024-08-07 10:42:24 +0530

    create rg

--- main.tf
+++ main.tf
@@ -2,19 +2,24 @@ terraform {
   required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
   cloud {
     organization = "vaboyaTerraformOrg"
     workspaces {
-      name = "VaboyaTerraformRepo"
       project = "TerraformLearnProject"
+      name    = "VaboyaTerraformRepo"      
     }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "justRG"
+  location = "eastus2"  
 }
