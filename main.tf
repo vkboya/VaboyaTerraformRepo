4de3ba1092f65577f34a3145be67245920080a61
root <root@LAPTOP-8SB38NT7>
2024-08-07 10:30:12 +0530

    moving code

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "vaboyaTerraformOrg"
+    workspaces {
+      name = "VaboyaTerraformRepo"
+      project = "TerraformLearnProject"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
